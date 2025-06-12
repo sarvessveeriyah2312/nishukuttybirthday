500d215158bad91af3c46cf69ae08abbdc6edcf6
Sarvess Veeriyah <49172946+sarvessveeriyah2312@users.noreply.github.com>
2025-06-12 11:16:27 +0800

    update the date

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,7 @@ export default function Home() {
   const [showBirthday, setShowBirthday] = useState(false);
   
   // Set your girlfriend's birthday date here
-  // const birthdayDate = new Date('2025-06-14T00:00:00');
-  const birthdayDate = new Date('');
+  const birthdayDate = new Date('2025-06-15T00:00:00');
   
   // For testing purposes, you can set a date just a few seconds in the future:
   // const birthdayDate = new Date(Date.now() + 10000); // 10 seconds from now
